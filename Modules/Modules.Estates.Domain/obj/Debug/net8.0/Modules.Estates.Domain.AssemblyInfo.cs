e38c98d9722fa151530312c3f53c367b9b215e17
Boakye-tech <boakye@gmail.com>
2025-03-09 01:37:31 +0000

    Merge Resolution

--- Modules/Modules.Estates.Domain/obj/Debug/net8.0/Modules.Estates.Domain.AssemblyInfo.cs
+++ Modules/Modules.Estates.Domain/obj/Debug/net8.0/Modules.Estates.Domain.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modules.Estates.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2715d98cd31c80437313c24625672d2939eb9212")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c225d4c6585206b0e5227d3258cab72b986cc27")]
->>>>>>> c2abe003589161cc732e22bbb6c7909bff9238e7
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54907416ad9ae6bf3e9a7654c609db3f9b73401f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modules.Estates.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modules.Estates.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
